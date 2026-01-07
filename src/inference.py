31356201214a86a25d7f0889600ba6604157fd61
EternalSyntaxError <146186588+EternalSyntaxError@users.noreply.github.com>
2026-01-07 10:49:55 -0500

    Minor refactors in inference.py and updated gitignore to ignore .vscode

--- src/inference.py
+++ src/inference.py
@@ -9,8 +9,8 @@ class Inference:
                  model_path: str,
                  delegate_options: dict = { 'backend_type' : 'htp'},
                  delegate_library: str = "libQnnTFLiteDelegate.so") -> None:
-        self.delegate_options = { 'backend_type' : 'htp'}
-        self.delegate = load_delegate(library=delegate_library, options=delegate_options)
+        self.delegate_options = delegate_options
+        self.delegate = load_delegate(library=delegate_library, options=self.delegate_options)
 
         self.interpreter = Interpreter(model_path=model_path, experimental_delegates=self.delegate)
         self.interpreter.allocate_tensors()
